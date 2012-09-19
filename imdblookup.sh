6419e1923dcf2a25b3a171d97443393813a3795a
yvesf <yvesf-git@xapek.org>
2012-09-19 07:43:04 +0000

    use http://www.deanclatworthy.com/imdb
    imdbapi.com is shut down by the imdb-police

--- imdblookup.sh
+++ imdblookup.sh
@@ -5,25 +5,34 @@ fs_clean() {
 }
 
 clean_name() {
-    echo "$1" | perl -pe 's/(.*?)\.(?:20[0-1][0-9]|19[0-9]{2}|dvd|ac3|R5|unrated|TS|720p|md|ts|ld|bdrip|tvrip|dvdrip|dvdscr|uncut|German|telesync)\..*/\1/i' | 
+    echo "$1" | perl -pe 's/(.*?)\.(?:20[0-1][0-9]|19[0-9]{2}|hdtv|extended|dvd|ac3|R2|R5|unrated|TS|720p|md|ts|ld|bdrip|tvrip|dvdrip|dvdscr|uncut|German|telesync)\..*/\1/i' | 
         perl -pe 's/\./ /g'
 }
 
 query() {
     file=$(mktemp -t imdblookup.XXX)
-    wget "http://www.imdbapi.com/?i=&t=$1" -O - -q |
+    term="$1"
+    year=$2
+    if [ -n "$year" ]; then
+	  term="${term}&year=$year"
+    fi
+    echo SEARCH: $term >&2
+    wget "http://www.deanclatworthy.com/imdb/?q=$term" -O - -q |
     	sed -e 's/",/"\
 /g; s/^{//; s/}$//; s/":/	/g; s/"//g' >$file
 
-    if grep "Response	True" $file >/dev/null; then
-        title="`grep -E '^Title' $file | cut -f 2 -d '	' | fs_clean`"
-        year="`grep -E '^Year' $file | cut -f 2 -d '	'`"
-        imdbId="`grep -E '^ID' $file | cut -f 2 -d '	'`"
+    if ! grep "\"code\":1," $file >/dev/null; then
+        title="`grep -E '^title' $file | cut -f 2 -d '	' | fs_clean`"
+        year="`grep -E '^year' $file | cut -f 2 -d '	'`"
+        imdbId="`grep -E 'imdbid' $file | cut -f 2 -d '	'`"
         echo "$title ($year) #$imdbId"
+        rm $file
+    	return 0
     else
         echo "Nothing found"
-    fi
     rm $file
+    	return 1
+    fi
 }
 
 imdb_get_year() {
@@ -34,5 +43,13 @@ imdb_get_year() {
 
 name=$(basename "$1")
 name=$(clean_name "$name")
-
-query "$name"
+name=$(query "$name" "$2")
+if [ "$?" = 0 ] && [ -d "$1" ]; then
+  echo "EXECUTE: ?"
+  echo "mkdir \"$name\""
+  echo "mv \"$1\" \"$name\""
+  read foo && ( test -d "$name" || mkdir "$name" ) && mv "$1" "$name"
+else
+  echo "mkdir \"$name\""
+  read foo && ( test -d "$name" && echo "Directory exists" || mkdir "$name" )
+fi
