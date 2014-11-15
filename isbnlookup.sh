148c6a465bbbaa7e52f0e16d9fc28f665b51fd4e
Yves Fischer <yvesf-git@xapek.org>
2014-11-15 16:33:03 +0100

    some changes..
    did them some time ago, dont' remember exactly

--- isbnlookup.sh
+++ isbnlookup.sh
@@ -11,17 +11,33 @@ extract_first_pages() {
 
 find_first_isbn_number() {
     grep "ISBN" \
-    | tr -c -d 'A-Za-z0-9\-\ ' \
+    | sed -e '
+# Unicode replacements
+s/\xef\x99\x81/9/g
+s/\xef\x99\x80/8/g
+s/\xef\x98\xbf/7/g
+s/\xef\x98\xbe/6/g
+s/\xef\x98\xbd/5/g
+s/\xef\x98\xbc/4/g
+s/\xef\x98\xbb/3/g
+s/\xef\x98\xba/2/g
+s/\xef\x9b\x9c/1/g
+s/\xef\x98\xb9/0/g
+# isbn-10 isbn-13.. prefix
+s/isbn-1[03]//i' \
     | head -n 1 \
-    | tr -c -d '0-9\-X'
+    | tr -c -d '0-9\-\ ' \
+    | sed -ne '
+:isbn13
+s/.*\([0-9]\{3\}-\?[0-9]-\?[0-9]\{3,4\}-\?[0-9]\{4\}-\?[0-9]-\?[0-9]\?\).*/isbn13 \1/p ; t finish;
+:isbn10
+s/.*\([0-9]\{1\}-\?[0-9]\{3,4\}-\?[0-9]\{4,5\}\).*/isbn10 \1/p ; t finish;
+:finish'
 }
-#  | grep -o -E "ISBN *.{0,10} *([0-9\-]{13,17}|[0-9]{10,13})" \
-#  | grep -o -E "([0-9\-]{13,17}|[0-9]{10,13})" \
 
 fetch_google_books_info() {
-    isbn=$1
     tempfile=`mktemp`
-    wget -O "$tempfile" -q "http://books.google.com/books/feeds/volumes?q=$1&start-index=11&max-results=10"
+    wget -O "$tempfile" -q "http://books.google.com/books/feeds/volumes?q=$1&max-results=10"
     echo $tempfile
 }
 
@@ -63,9 +79,8 @@ google_books_identifier() {
 }
 
 sanitize_filename() {
-    echo "${1}" | tr -c -d 'A-Za-z0-9\ '
+    echo "${1}" | tr -c -d 'A-Za-z0-9:\ '
 }
-
 
 if [ "$1" = "-r" ]; then
     rename=true
@@ -76,25 +91,35 @@ fi
 search=$*
 
 if [ -f "$search" ]; then
+    echo -n "search file: $search"
     isbn=`extract_first_pages "${search}" | find_first_isbn_number`
     if [ ${#isbn} -eq 0 ]; then
-        error "No ISBN found in ${search}"
+        echo " ... no isbn found"
+        exit 1
     fi
-    infofile=`fetch_google_books_info "isbn:$isbn"`
+    echo " ... $isbn"
+    infofile=`fetch_google_books_info "${isbn##* }"`
 else
     echo "search term: $search"
     infofile=`fetch_google_books_info "$search"`
 fi
 
-if [ `google_books_count_results "$infofile"` -eq 1 ]; then
-    number=1
-elif [ `google_books_count_results "$infofile"` -eq 0 ]; then
+#if [ `google_books_count_results "$infofile"` -eq 1 ]; then
+#    number=1
+#el
+if [ `google_books_count_results "$infofile"` -eq 0 ]; then
     error "Nothing found for \"$search\" / $isbn"
 else
     google_books_print_results $infofile
-    echo -n "Pick Number: "
+    echo -n "Results for >$search< Pick Number: "
     read number
 fi
+
+if [ -z "$number" ]; then
+    echo "Skip this file"
+    exit 0
+fi
+
 author=`google_books_authors "$infofile" $number`
 title=`google_books_title "$infofile" $number`
 identifier=`google_books_identifier "$infofile" $number`
@@ -103,11 +128,16 @@ if [ -n "$author" ]; then
 else
     newname="$title #isbn_$identifier.pdf"
 fi
-newname=`echo "$newname" | tr -d -c 'A-Za-z0-9. #_,:-'`
+newname=`echo "$newname" | tr -d -c 'A-Za-z0-9. #_,-'`
 
 if $rename && [ -f "$search" ]; then
-    mv -v "$search" "$newname"
+    echo "=> => => Rename: (press return or ctrl-c)"
+    echo "<=  $search"
+    echo " => $newname"
+#    read
+    mv "$search" "$newname"
+    mv "$infofile" "$newname.googlebooks.xml"
 else
     echo $newname
+    rm $infofile
 fi
-rm $infofile
